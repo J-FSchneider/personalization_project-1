8c6b54fbdaa03b6fe62db9e7ca154f84ff416242
Maskani Filali Mohamed <maskani.mohamed@gmail.com>
2017-10-24 15:59:13 -0400

    Fix CleanTransformer class

--- utils/CleanTransformer.py
+++ utils/CleanTransformer.py
@@ -1,4 +1,4 @@
-from .preprocessing import parse_release_date, parse_ts_listen
+from utils.preprocessing import parse_release_date, parse_ts_listen
 
 class CleanTransformer():
     """
@@ -24,19 +24,27 @@ class CleanTransformer():
     def fit_transform(self, data):
         self.fit(data)
         self.transform()
+        return self
 
     # TODO: add other features to clean if needed
     def clean_ts_listen(self):
-        self.data = self.data[2011 <= self.data["year_listen"] <= 2017]
-        self.data = self.data[1 <= self.data["month_listen"] <= 12]
-        self.data = self.data[1 <= self.data["day_listen"] <= 31]
-        self.data = self.data[0 <= self.data["hour_listen"] <= 23]
+        self.data = self.data[(2011 <= self.data["year_listen"]) &
+                              (self.data["year_listen"] <= 2017)]
+        self.data = self.data[(1 <= self.data["month_listen"]) &
+                              (self.data["month_listen"] <= 12)]
+        self.data = self.data[(1 <= self.data["day_listen"]) &
+                              (self.data["day_listen"] <= 31)]
+        self.data = self.data[(0 <= self.data["hour_listen"]) &
+                              (self.data["hour_listen"] <= 23)]
 
     def clean_release_date(self):
         # TODO: verify min and max year release allowed
-        self.data = self.data[2011 <= self.data["year_listen"] <= 2017]
-        self.data = self.data[1 <= self.data["month_release"] <= 12]
-        self.data = self.data[1 <= self.data["day_release"] <= 31]
+        self.data = self.data[(1950 <= self.data["year_release"]) &
+                              (self.data["year_release"] <= 2017)]
+        self.data = self.data[(1 <= self.data["month_release"]) &
+                              (self.data["month_release"] <= 12)]
+        self.data = self.data[(1 <= self.data["day_release"]) &
+                              (self.data["day_release"] <= 31)]
 
     def remove_outliers(self):
         # TODO: to implement
@@ -47,3 +55,8 @@ class CleanTransformer():
             raise IOError("You need first to transform your dataframe.")
         self.clean_ts_listen()
         self.clean_release_date()
+        return self.data
+
+    def dump(self):
+        # TODO
+        pass
