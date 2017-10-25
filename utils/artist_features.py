7f6ad62f9c000d40c5672008aad36ff7cff2c811
Maskani Filali Mohamed <maskani.mohamed@gmail.com>
2017-10-25 16:34:30 -0400

    Fixes and adds TODOs in artist_feature module

--- utils/artist_features.py
+++ utils/artist_features.py
@@ -1,4 +1,3 @@
-# File description
 """
     Here are some functions that take the Dreezer data set and
     create a new table at the artist level that incorporates
@@ -15,19 +14,30 @@ import pandas as pd
 def artist_pop(df, target, column):
     """
     This function constructs popularity metrics for the artist from
-    Dreezer's database. The first popularity metric is "row_pop"
+    the Deezer dataset.
+
+    The first popularity metric is "row_pop"
     which is a notion of popularity based on the number of times
     that the artist was played (in other words the row count).
-    The second metric is a notion of how many distinct users
-    was the artist listened by (the unique user count)
 
-    :param df: Dreezer's database in the DataFrame format
-    :param target: the name of the identifier columns
-    :param column: the name of the column where the unique count
-    is gong to be perform
-    :return master_table: a DataFrame at the artist level that
-    contains the popularity features
+    The second metric is a notion of how many distinct users
+    was the artist listened by (the unique user count).
+
+    :param df: pd.DataFrame | DataFrame containing the columns
+                              "target" and "column"
+    :param target: str | The name of the identifier columns
+    :param column: The name of the column where the unique count
+                   is gong to be perform
+    :return master_table: pd.DataFrame | DataFrame at the artist level
+                          that contains the popularity features
     """
+    # TODO: change name of parameter column as it can be confusing
+
+    if target not in df or column not in df:
+        raise IOError("One of the column names entered is not contained"
+                      "in the dataframe.")
+
+    # TODO: Delete intermediary variables, unecessary
     new_column1 = "row_pop"
     criteria1 = "count"
     var_agg = {target: criteria1}
@@ -36,6 +46,7 @@ def artist_pop(df, target, column):
     row_pop = row_pop.reset_index()
     master_table = row_pop.sort_values(new_column1, ascending=False)
 
+    # TODO: Delete intermediary variables, unecessary
     new_column2 = "user_pop"
     criteria2 = "nunique"
     var_agg = {column: criteria2}
@@ -43,20 +54,32 @@ def artist_pop(df, target, column):
     user_pop.columns = [new_column2]
     user_pop = user_pop.reset_index()
 
-    master_table = pd.DataFrame.merge(master_table, user_pop, on=target, how="left")
+    master_table = pd.DataFrame.merge(master_table, user_pop, on=target,
+                                      how="left")
     return master_table
 
 
 def popularity_cat(df, target, column):
     """
-    This function generates the categorical columns for the artist's popularity features
-    by first generating categories based on certain thresholds and then pivoting
-    them into new columns
-    :param df: Dreezer's database in the DataFrame format
-    :param target: the name of the identifier columns
-    :param column: the name of the column where the threshold is going to be applied
-    :return df: a DataFrame with the artist's identifier and the category columns
+    This function generates the categorical columns for the artist's
+    popularity features by first generating categories based on certain
+    thresholds and then pivoting them into new columns.
+
+    :param df: pd.DataFrame | DataFrame containing the columns
+                              "target" and "column"
+    :param target: str | The name of the identifier columns
+    :param column: str | The name of the column where the threshold
+                         is going to be applied
+    :return df: pd.DataFrame | DataFrame with the artist's identifier
+                               and the category columns
     """
+    # TODO: change name of parameter column as it can be confusing
+
+    if target not in df or column not in df:
+        raise IOError("One of the column names entered is not contained"
+                      "in the dataframe.")
+
+    # TODO: Delete intermediary variables, unecessary
     n = len(df)
     cat1 = "unpop"
     cat2 = "known"
@@ -67,12 +90,20 @@ def popularity_cat(df, target, column):
     cat_column = "artist"
     df[cat_column] = cat1
     cat_pos = df.columns.get_loc(cat_column)
+    # TODO: .iloc is very inefficient, and no need to use for loop, instead use pandas' slicing functions
+    """
     for i in range(n):
         if df.iloc[i, pos] > threshold2:
             df.iloc[i, cat_pos] = cat3
         elif (df.iloc[i, pos] > threshold1) & (df.iloc[i, pos] <= threshold2):
             df.iloc[i, cat_pos] = cat2
+    """
+    # pop artists
+    df.loc[df[column] > threshold1, cat_column] = cat2
+    # know artists
+    df.loc[df[column] > threshold2, cat_column] = cat3
 
+    # TODO: remove unecessary temp variables, just makes more lines to read
     column_selection = [target, cat_column]
     df = df[column_selection]
     df = pd.get_dummies(df)
@@ -82,15 +113,17 @@ def popularity_cat(df, target, column):
 
 def art_hits(df, target, column, threshold=0.75):
     """
-    This function calculates the number of  songs that generate a certain percentage of the
-    total number of songs that the artist was played upon.
-    :param df: Dreezer's database in the DataFrame format
+    This function calculates the number of  songs that generate a certain
+    percentage of the total number of songs that the artist was played upon.
+
+    :param df: Deezer's database in the DataFrame format
     :param target: the name of the identifier columns
     :param column: the name of the column where filtering is going to take place
     :param threshold: is the percentage of interest
     :return result: a DataFrame which contains the artist's id and the no. of songs that
     pass the threshold
     """
+    # TODO: take into consideration the previous comments for this function too
     criteria1 = "count"
     criteria2 = "sum"
     new_col = "song_count"
@@ -119,6 +152,7 @@ def art_hits(df, target, column, threshold=0.75):
     per_pos = hits.columns.get_loc(per_col)
     hits[flag_col] = 1
     flag_pos = hits.columns.get_loc(flag_col)
+    # TODO: as in previous functions, get rid of .iloc
     for i in range(1, n, 1):
         if hits.iloc[i, id_pos] == hits.iloc[i - 1, id_pos]:
             hits.iloc[i, acc_pos] = hits.iloc[i - 1, acc_pos] \
@@ -148,6 +182,7 @@ def hits_cat(df, target):
     :param target: the name of the identifier columns
     :return df:a DataFrame with the artist's identifier and the hits category columns
     """
+    # TODO: take into consideration the previous comments for this function too
     column = "no_songs"
     n = len(df)
     cat1 = "one"
@@ -159,6 +194,7 @@ def hits_cat(df, target):
     cat_column = "hits"
     df[cat_column] = cat1
     cat_pos = df.columns.get_loc(cat_column)
+    # TODO: same here
     for i in range(n):
         if df.iloc[i, pos] > threshold2:
             df.iloc[i, cat_pos] = cat3
@@ -170,3 +206,6 @@ def hits_cat(df, target):
     df = pd.get_dummies(df)
 
     return df
+
+if __name__ == "__main__":
+    #TODO: show how these functions can be used to generate the desired df
