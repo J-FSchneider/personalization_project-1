f72cbdfac19096c323b332c64e6ae0bdee6608ef
AndPotap <apotapczynski@gmail.com>
2017-10-25 14:04:47 -0400

    added the hits features for the artists table

--- artist_features.py
+++ artist_features.py
@@ -20,7 +20,7 @@ def artist_pop(df, target, column):
     that the artist was played (in other words the row count).
     The second metric is a notion of how many distinct users
     was the artist listened by (the unique user count)
-    
+
     :param df: Dreezer's database in the DataFrame format
     :param target: the name of the identifier columns
     :param column: the name of the column where the unique count
@@ -45,3 +45,128 @@ def artist_pop(df, target, column):
 
     master_table = pd.DataFrame.merge(master_table, user_pop, on=target, how="left")
     return master_table
+
+
+def popularity_cat(df, target, column):
+    """
+    This function generates the categorical columns for the artist's popularity features
+    by first generating categories based on certain thresholds and then pivoting
+    them into new columns
+    :param df: Dreezer's database in the DataFrame format
+    :param target: the name of the identifier columns
+    :param column: the name of the column where the threshold is going to be applied
+    :return df: a DataFrame with the artist's identifier and the category columns
+    """
+    n = len(df)
+    cat1 = "unpop"
+    cat2 = "known"
+    cat3 = "pop"
+    pos = df.columns.get_loc(column)
+    threshold1 = 15
+    threshold2 = 157
+    cat_column = "artist"
+    df[cat_column] = cat1
+    cat_pos = df.columns.get_loc(cat_column)
+    for i in range(n):
+        if df.iloc[i, pos] > threshold2:
+            df.iloc[i, cat_pos] = cat3
+        elif (df.iloc[i, pos] > threshold1) & (df.iloc[i, pos] <= threshold2):
+            df.iloc[i, cat_pos] = cat2
+
+    column_selection = [target, cat_column]
+    df = df[column_selection]
+    df = pd.get_dummies(df)
+
+    return df
+
+
+def art_hits(df, target, column, threshold=0.75):
+    """
+    This function calculates the number of  songs that generate a certain percentage of the
+    total number of songs that the artist was played upon.
+    :param df: Dreezer's database in the DataFrame format
+    :param target: the name of the identifier columns
+    :param column: the name of the column where filtering is going to take place
+    :param threshold: is the percentage of interest
+    :return result: a DataFrame which contains the artist's id and the no. of songs that
+    pass the threshold
+    """
+    criteria1 = "count"
+    criteria2 = "sum"
+    new_col = "song_count"
+    var_agg = {column: criteria1}
+    hits = df.groupby(by=[target, column]).agg(var_agg)
+    hits.columns = [new_col]
+    hits = hits.reset_index()
+    hits = hits.sort_values([target, new_col], ascending=[1, 0])
+
+    var_agg = {new_col: criteria2}
+    total = hits.groupby(by=target).agg(var_agg)
+    total_col = "tot_count"
+    total.columns = [total_col]
+    total = total.reset_index()
+
+    hits = hits.merge(total, on=target, how="left")
+    per_col = "per_tot"
+    hits[per_col] = hits[new_col] / hits[total_col]
+
+    n = len(hits)
+    id_pos = hits.columns.get_loc(target)
+    acc_col = "cul_tot"
+    flag_col = "flag"
+    hits[acc_col] = hits[per_col]
+    acc_pos = hits.columns.get_loc(acc_col)
+    per_pos = hits.columns.get_loc(per_col)
+    hits[flag_col] = 1
+    flag_pos = hits.columns.get_loc(flag_col)
+    for i in range(1, n, 1):
+        if hits.iloc[i, id_pos] == hits.iloc[i - 1, id_pos]:
+            hits.iloc[i, acc_pos] = hits.iloc[i - 1, acc_pos] \
+                                    + hits.iloc[i, per_pos]
+
+    for j in range(n - 1):
+        if hits.iloc[j, id_pos] == hits.iloc[j - 1, id_pos]:
+            if (hits.iloc[j, acc_pos] > threshold) & (hits.iloc[j, acc_pos] < 1):
+                hits.iloc[j + 1, flag_pos] = 0
+
+    criteria = "sum"
+    column = "flag"
+    var_agg = {column: criteria}
+    result = hits.groupby(by=target).agg(var_agg)
+    result.columns = ["no_songs"]
+    result = result.reset_index()
+
+    return result
+
+
+def hits_cat(df, target):
+    """
+    This function generates the categorical columns for the artist's "hits" features
+    by first generating categories based on certain thresholds and then pivoting
+    them into new columns
+    :param df: Dreezer's database in the DataFrame format
+    :param target: the name of the identifier columns
+    :return df:a DataFrame with the artist's identifier and the hits category columns
+    """
+    column = "no_songs"
+    n = len(df)
+    cat1 = "one"
+    cat2 = "medium"
+    cat3 = "many"
+    pos = df.columns.get_loc(column)
+    threshold1 = 3
+    threshold2 = 15
+    cat_column = "hits"
+    df[cat_column] = cat1
+    cat_pos = df.columns.get_loc(cat_column)
+    for i in range(n):
+        if df.iloc[i, pos] > threshold2:
+            df.iloc[i, cat_pos] = cat3
+        elif (df.iloc[i, pos] > threshold1) & (df.iloc[i, pos] <= threshold2):
+            df.iloc[i, cat_pos] = cat2
+
+    column_selection = [target, cat_column]
+    df = df[column_selection]
+    df = pd.get_dummies(df)
+
+    return df
