29ae99d9227d3bfbcffbf001e682407e3725a86c
Maskani Filali Mohamed <maskani.mohamed@gmail.com>
2017-10-25 17:24:41 -0400

    Quick fixes and TODOs in matrix factorization model

--- models/model_based/matrix_creation.py
+++ models/model_based/matrix_creation.py
@@ -1,32 +1,36 @@
 import numpy as np
 import pandas as pd
+# TODO: unused imports to be deleted
 
+def sample_data_by_frequency(data):
+    # TODO: Add standard docstring
+    # This function returns the items and users with the most frequency in the is_listened column
 
-def sample_data_by_frequency(data)
-	# This function returns the items and users with the most frequency in the is_listened column
+    # List of unique users (with media count)
+    user_rating_count = data.groupby('user_id')['media_id'].nunique()
+    # List of users in ascending order (by number of songs, not unique, listened to)
+    user_rating_count = user_rating_count.sort_values(axis=0, ascending=False)
+    # select 10,000 most active users
+    # TODO: check variable u_sel never used
+    u_sel = user_rating_count[:10000]
+    # return list with user ids
+    user = user_rating_count.index
 
-	# List of unique users (with media count)
-	user_rating_count = data.groupby('user_id')['media_id'].nunique()
-	# List of users in ascending order (by number of songs, not unique, listened to)
-	user_rating_count = user_rating_count.sort_values(axis=0, ascending=False)
-	# select 10,000 most active users
-	u_sel = user_rating_count[:10000]
-	# return list with user ids
-	user = user_rating_count.index
+    # List of unique media ids (with user count)
+    media_count = data.groupby('media_id')['user_id'].nunique()
+    # List of media in ascending order (by number of users)
+    media_count = media_count.sort_values(axis=0, ascending=False)
+    # Select top 100 items
+    # TODO: check variable m_sel never used
+    m_sel = media_count[:100]
+    # return list with media_ids
+    media = media_count.index
 
-	# List of unique media ids (with user count)
-	media_count = data.groupby('media_id')['user_id'].nunique()
-	# List of media in ascending order (by number of users)
-	media_count = media_count.sort_values(axis=0, ascending=False)
-	# Select top 100 items
-	m_sel = media_count[:100]
-	# return list with media_ids
-	media = media_count.index
-
-	return user, media
+    return user, media
 
 
 def rating_matrix(data, user, media):
+    # TODO: Add standard docstring
 	# returns a rating matrix in wide format given a long format table, sampled by the specified user and media ids
     # data: the raw_data to build the matrix from
     # user: an df of user_ids to select from data
@@ -39,10 +43,12 @@ def rating_matrix(data, user, media):
     items = users[users['media_id'].isin(media)]
 
     # drop duplicates (assumption: keep the first of an item)
-    matrix = items.drop_duplicates(subset=['media_id','user_id'],keep='first')
+    matrix = items.drop_duplicates(subset=['media_id', 'user_id'], keep='first')
 
     # get long format into wide format
-    matrix = matrix.pivot(index='user_id', columns='media_id', values='is_listened')
+    # TODO: check memory usage, improve to sparse matrix storage if necessary
+    matrix = matrix.pivot(index='user_id',
+                          columns='media_id',
+                          values='is_listened')
 
     return matrix
-    
