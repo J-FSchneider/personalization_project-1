5384978223b5c6bba9d13577a731083ecd0ca9e3
Maskani Filali Mohamed <maskani.mohamed@gmail.com>
2017-10-25 16:52:47 -0400

    Quick fixes in categorical_encoded and TODOs

--- utils/Categorical_encoded.py
+++ utils/Categorical_encoded.py
@@ -1,62 +1,60 @@
 from utils.preprocessing import *
 
+
 def time_of_day_encoded(data):
     """
-    Converts the moment of day buckets to categorical varibales in different columns.
-    Input: pandas DataFrame
-    Output: Appended dataframe(Use data = time_of_day_categorical(data) to change the
-    existing Dataframe)
-    
+    Convert "time_of_day" variable into dummy/indicator variables
+    :param data: pd.DataFrame
+    :return: pd.DataFrame | input dataframe with additional columns
     """
-    parse_ts_listen(data) #Preprocess
-    parse_moment_of_day(data) #Preprocess
-   if "moment_of_day" not in data:
+    # TODO: check redundant work with CleanTransformer instances
+    parse_ts_listen(data)
+    parse_moment_of_day(data)
+
+    # TODO: this check is useless then if you call the function above
+    if "moment_of_day" not in data:
         raise IOError("The input dataframe does not contain "
                       "the column 'moment_of_day'")
-      #Making the dummy variable from the categorical 'moment_of_day'   
-    temp1 = pd.get_dummies(data.moment_of_day)
-    
-      #Joining the new dataset with the encoding to the original data
-    data = pd.concat([data,temp1],axis =1) 
-    
-    del temp1
-    
+
+    data = pd.concat([data, pd.get_dummies(data.moment_of_day)], axis=1)
+
     return data
 
 
 def age_bucket_encoded(data):
+    """
+    Convert "age_bucket" variable into dummy/indicator variables
+    :param data: pd.DataFrame
+    :return: pd.DataFrame | input dataframe with additional columns
+    """
+    parse_user_age(data)
 
-    """
-    Converts the age buckets to categorical varibales in different columns.
-    Input: pandas DataFrame
-    Output: Appended dataframe(Use data = age_categorical(data) to change the
-    existing Dataframe)
-    
-    """
-	parse_user_age(data) #preprocess
-   
+    # TODO: this check is useless then if you call the function above
     if "user_age_bucket" not in data:
         raise IOError("The input dataframe does not contain "
                       "the column 'user_age_bucket'")
-        
-     #Making the dummy variable from the categorical 'user_age_bucket'   
-    temp = pd.get_dummies(data.user_age_bucket)
+
     # Joining the new dataset with the encoding to the original data
-    data = pd.concat([data, temp],axis =1) 
-    
-    del temp
-    
+    data = pd.concat([data, pd.get_dummies(data.user_age_bucket)], axis=1)
+
     return data
 
-def modified_data(data) :	
+# TODO: change name of function to more explicit one
+def modified_data(data) :
     """
     This function calls the functions mentioned above and stores the values in the dataset 
     to output the new dataset with the encoded values. It also deletes the 
     'user_age_bucket' and 'moment_of_day' column as they are not required anymore
     """
-  data = time_of_day_encoded(data) #storing new dataset
-  data = age_bucket_encoded(data) #storing new dataset
-  del data['user_age_bucket'] #removing the 'user_age_bucket' column
-  del data['moment_of_day'] #Removing the 'moment_of_day' column
-  
-  return data
+    # TODO: change docstring above to a standard one
+    data = time_of_day_encoded(data) #storing new dataset
+    data = age_bucket_encoded(data) #storing new dataset
+    # TODO: use data.drop instead of del
+    del data['user_age_bucket'] #removing the 'user_age_bucket' column
+    del data['moment_of_day'] #Removing the 'moment_of_day' column
+
+    # TODO: are you sure that is the final dataframe one can use to do the K-NN analysis?
+    return data
+
+if __name__ == "__main__":
+    #TODO: show how these functions can be used to generate the desired df from scratch
