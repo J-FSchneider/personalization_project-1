a5bfd1e0959c6416b6f79e2924647c2466617a08
Maskani Filali Mohamed <maskani.mohamed@gmail.com>
2017-11-03 14:09:47 -0400

    Ident properly

--- utils/loss_functions.py
+++ utils/loss_functions.py
@@ -1,9 +1,9 @@
 def mean_squared_error(prediction, test_set):
-    """
-    Calculates the mean squared error between the test_set and the prediction
-    :params prediction: dict | input of form: {(i,u): value}
-    :params test_set: dict | input of form: {(i,u): value}
-    """
+	"""
+	Calculates the mean squared error between the test_set and the prediction
+	:params prediction: dict | input of form: {(i,u): value}
+	:params test_set: dict | input of form: {(i,u): value}
+	"""
 	error = 0
 	for i in test_set.keys():
 		i_error = (prediction[i] - test_set[i])**2
@@ -11,14 +11,14 @@ def mean_squared_error(prediction, test_set):
 	error = error/len(test_set.keys())
 	print("MSE: " + str(error))
 	return error
- 
+
 
 def absolute_mean_error(prediction, test_set):
-    """
-    Calculates the absolute mean error between the test_set and the prediction
-    :params prediction: dict | input of form: {(i,u): value}
-    :params test_set: dict | input of form: {(i,u): value}
-    """
+	"""
+	Calculates the absolute mean error between the test_set and the prediction
+	:params prediction: dict | input of form: {(i,u): value}
+	:params test_set: dict | input of form: {(i,u): value}
+	"""
 	error = 0
 	for i in test_set.keys():
 		i_error = abs(prediction[i] - test_set[i])
