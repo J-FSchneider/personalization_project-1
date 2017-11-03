932bdb8cec8cdb20bbf9bcc381eea168649f64d2
Jan-Felix Schneider <jan-felix.schneider@gmx.de>
2017-11-03 13:05:19 -0400

    Quick fixes in loss_functions

--- utils/loss_functions.py
+++ utils/loss_functions.py
@@ -4,17 +4,14 @@ def mean_squared_error(prediction, test_set):
     :params prediction: dict | input of form: {(i,u): value}
     :params test_set: dict | input of form: {(i,u): value}
     """
-
-
-
 	error = 0
 	for i in test_set.keys():
 		i_error = (prediction[i] - test_set[i])**2
 		error += i_error
 	error = error/len(test_set.keys())
-	print "MSE: " + str(error)
+	print("MSE: " + str(error))
 	return error
-
+ 
 
 def absolute_mean_error(prediction, test_set):
     """
@@ -27,5 +24,5 @@ def absolute_mean_error(prediction, test_set):
 		i_error = abs(prediction[i] - test_set[i])
 		error += i_error
 	error = error/len(test_set.keys())
-	print "AME: " + str(error)
+	print("AME: " + str(error))
 	return error
