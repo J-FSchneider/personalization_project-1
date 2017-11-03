7c9be233bafe4cec63e860440d0288cf73149658
manksh <manksh.gupta@gmail.com>
2017-11-03 17:00:33 -0400

    add precision in module

--- utils/loss_functions.py
+++ utils/loss_functions.py
@@ -26,3 +26,30 @@ def absolute_mean_error(prediction, test_set):
 	error = error/len(test_set.keys())
 	print("AME: " + str(error))
 	return error
+
+def precision(prediction, test_set, threshhold = 0.5):
+	"""
+	Calculates the precision between the test_set and the prediction
+
+	:param prediction: dict | input of form: {(i,u): value}
+	:param test_set: input of form: {(i,u): value}
+	:param threshhold: Probability threshhold for prediction
+	:return: value of Precision
+	"""
+	for j in prediction.keys():
+		if prediction[j] >= threshhold:
+			prediction[j] = 1
+		else:
+			prediction[j] = 0
+	prec = 0
+	tp = 0
+	p = 0
+	for i in test_set.keys():
+		if prediction[i] == 1:
+			p = p + 1
+		if prediction[i] == test_set[i]:
+			tp = tp + 1
+	prec = tp / p
+
+	print("Precision: " + str(prec))
+	return prec
