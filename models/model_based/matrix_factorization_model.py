f91c944a33ed7568f89993db6a9dee84a3086031
Jan-Felix Schneider <jan-felix.schneider@gmx.de>
2017-11-05 22:45:17 -0500

    Add hyperparameter tuning function

--- models/model_based/matrix_factorization_model.py
+++ models/model_based/matrix_factorization_model.py
@@ -1,5 +1,6 @@
 import numpy as np 
 import pandas as pd
+from utils import loss_functions as lf
 
 # TODO: should create a class for this model (Mohamed will take care of it)
 
@@ -29,10 +30,15 @@ def simple_latent_factor_model(M,
                                latent_factors=10,
                                learning_rate=1e-6
                                ):
-    # TODO: two parameters not used
-    # TODO: Add standard docstring
-    # Calculates a latent factor model with give number of latent factors by gradient descent. 
-    # Returns the ratings prediction matrix and the latent factor matrices U (users) and V (items)
+    """
+    Calculates a latent factor model with give number of latent factors by gradient descent.
+    Returns the ratings prediction matrix and the latent factor matrices U (users) and V (items)
+    :param M:
+    :param latent_factors:
+    :param learning_rate:
+    :return:
+    """
+
 
     # Randomly initialize Matrix U and V
     m = M.shape[0]
@@ -77,7 +83,7 @@ def latent_factors_with_bias(M,
                             bias=None, 
                             bias_weights=None, 
                             regularization=0,
-                            learning_rate=0.000001, 
+                            learning_rate=0.0001,
                             convergence_rate =0.1
                             ):
     # Calculates a latent factor model with give number of latent factors by gradient descent. 
@@ -131,7 +137,11 @@ def latent_factors_with_bias(M,
     return R_hat, U, V
 
 def test_accuracy(M, R_hat):
-    # TODO: Add standard docstring
+    """
+    :param M:
+    :param R_hat:
+    :return:
+    """
     # returns the accuracy of the model predictions
 
     # turn probabilities into predictions
@@ -147,5 +157,87 @@ def test_accuracy(M, R_hat):
     return total_acc
 
 
+def hyper_parameter_tuning(data,
+                           ModelTester,
+                           latent_factors=5,
+                           regularization=1,
+                           bias=0.5,
+                           bias_weights=0.2,
 
+                           verbose=False):
+    """
+    :param data: df | rating matrix
+    :param ModelTester: ModelTester obj | Object with validation and test set
+    :param latent_factors: int |
+    :param regularization:  float |
+    :param bias: float |
+    :param bias_weights: float |
+    :return: list, list | two lists of ame and sme error
+    """
+    error_mse = []
+    error_ame = []
+    if type(latent_factors) == list:
+        for l in latent_factors:
+            predictions, U, V = latent_factors_with_bias(data,
+                                                        latent_factors=l,
+                                                        bias=bias,
+                                                        bias_weights=bias_weights,
+                                                        regularization=regularization,
+                                                        )
+            predictions = pd.DataFrame(predictions)
+            predictions.columns = data.columns
+            predictions.index = data.index
+            ame = ModelTester.evaluate_valid(predictions, loss_func=lf.absolute_mean_error, verbose=verbose)
+            mse = ModelTester.evaluate_valid(predictions, loss_func=lf.mean_squared_error, verbose=verbose)
+            error_mse.append(mse)
+            error_ame.append(ame)
 
+    elif type(regularization) == list:
+        for r in regularization:
+            predictions, U, V = latent_factors_with_bias(data,
+                                                        latent_factors=latent_factors,
+                                                        bias=bias,
+                                                        bias_weights=bias_weights,
+                                                        regularization=r,
+                                                        )
+            predictions = pd.DataFrame(predictions)
+            predictions.columns = data.columns
+            predictions.index = data.index
+            ame = ModelTester.evaluate_valid(predictions, loss_func=lf.absolute_mean_error, verbose=verbose)
+            mse = ModelTester.evaluate_valid(predictions, loss_func=lf.mean_squared_error, verbose=verbose)
+            error_mse.append(mse)
+            error_ame.append(ame)
+
+    elif type(bias) == list:
+        for b in bias:
+            predictions, U, V = latent_factors_with_bias(data,
+                                                         latent_factors=latent_factors,
+                                                         bias=b,
+                                                         bias_weights=bias_weights,
+                                                         regularization=regularization,
+                                                         )
+            predictions = pd.DataFrame(predictions)
+            predictions.columns = data.columns
+            predictions.index = data.index
+            ame = ModelTester.evaluate_valid(predictions, loss_func=lf.absolute_mean_error, verbose=verbose)
+            mse = ModelTester.evaluate_valid(predictions, loss_func=lf.mean_squared_error, verbose=verbose)
+            error_mse.append(mse)
+            error_ame.append(ame)
+
+    elif type(regularization) == list:
+        for bw in bias_weights:
+            predictions, U, V = latent_factors_with_bias(data,
+                                                        latent_factors=latent_factors,
+                                                        bias=bias,
+                                                        bias_weights=bw,
+                                                        regularization=regularization,
+                                                        )
+            predictions = pd.DataFrame(predictions)
+            predictions.columns = data.columns
+            predictions.index = data.index
+            ame = ModelTester.evaluate_valid(predictions, loss_func=lf.absolute_mean_error, verbose=verbose)
+            mse = ModelTester.evaluate_valid(predictions, loss_func=lf.mean_squared_error, verbose=verbose)
+            error_mse.append(mse)
+            error_ame.append(ame)
+
+    return error_mse, error_ame
