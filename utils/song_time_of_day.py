55fefa9d6887b670f6dbf7eee0e6e4dc0ae4763d
manksh <manksh.gupta@gmail.com>
2017-12-11 13:26:49 -0500

    add function for chi-square test for independence

--- utils/song_time_of_day.py
+++ utils/song_time_of_day.py
@@ -1,15 +1,17 @@
 from utils.preprocessing import *
 import warnings
+from scipy.stats import chi2_contingency
 
 
 def song_time_of_day(data, user_number, min_num_songs=2):
     """
+    The function calculates the ratio of common songs to total songs a given
+    user listens to during different time periods in the day
 
-
-    :param data: dataset to use
-    :param user_number: the user for which the information is required
-    :param min_num_songs: song cutoff for the user
-    :return:
+    :param data: pd.dataFrame| Data set to use
+    :param user_number: int| the user for which the information is required
+    :param min_num_songs: int|  song cutoff for the user
+    :return: The ratio of songs
     """
 
     warnings.filterwarnings("ignore")
@@ -22,7 +24,8 @@ def song_time_of_day(data, user_number, min_num_songs=2):
         parse_moment_of_day2(user)
 
     if min_num_songs >= max(user.groupby(['media_id', 'moment_of_day']).count()['ts_listen']) - 2:
-        raise IOError("Minimum number of songs is more than the maximum songs listened by the user")
+        raise IOError("Minimum number of songs is more "
+                      "than the maximum songs listened by the user")
 
     else:
 
@@ -31,7 +34,9 @@ def song_time_of_day(data, user_number, min_num_songs=2):
 
         df = df.reset_index()
 
-        user_pivot = df.pivot(index='moment_of_day', columns='media_id', values='genre_id')
+        user_pivot = df.pivot(index='moment_of_day',
+                              columns='media_id',
+                              values='genre_id')
 
         user_pivot = user_pivot.fillna(0)
 
@@ -39,8 +44,25 @@ def song_time_of_day(data, user_number, min_num_songs=2):
 
         tpose = user_pivot.transpose()
 
-        tpose['mult'] = tpose[tpose.column[0]] * tpose[tpose.column[1]]
+        tpose['mult'] = tpose[tpose.column[0]] * tpose[tpose.column[1]] * tpose[tpose.column[2]]
 
         percent = tpose.mult.sum() / user_pivot.shape[1]
 
         return percent
+
+
+def chisq_test(data, feature1, feature2):
+    """
+    Calculates the chi-square test of independence of two events.
+
+    :param data: pd.DataFrame | Data set for analysis
+    :param feature1: str | feature1 to check for independence
+    :param feature2: str | feature2 to check for independence
+    :return: returns the original contingency table, chi sq statistic, p value
+    degrees of freedom and the table of expected values
+    """
+
+    chi = pd.crosstab(data[feature1], df[feature2])
+    val = chi2_contingency(chi, correction=True, lambda_=None)
+
+    return chi, val
