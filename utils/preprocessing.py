d2c782277cfbef793d76255d6e4bc649d120af64
Jan-Felix Schneider <jan-felix.schneider@gmx.de>
2017-12-14 22:07:39 -0500

    Add survey parsing function

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -509,6 +509,88 @@ def parse_media_duration_bucket(data):
         map(get_media_duration_bucket)
 
 
+def join_survey_data(survey, deezer):
+    """
+    Parse the survey data into the deezer dataset. 
+    :param survey: pd.df | the survey data
+    :param deezer: pd.df | the deezer data
+    :return: pd.df | the deezer data with the parsed survey data
+    """
+    df = survey.rename(
+        columns={'Name': 'user_id', 'Age': 'user_age', 'Gender': 'user_gender',
+                 'deezer_id': 'media_id'})
+
+    for index, row in df.iterrows():
+        if pd.isnull(row['time']):
+            continue
+        time = row['time'].split(',')
+        if row['user_gender'] == 'Male':
+            user_gender = 1
+        else:
+            user_gender = 0
+        print(time)
+        if time == None:
+            if row['rating'] == 0:
+                for i in [1480513129, 1479067262, 1478675619]:
+                    new = pd.DataFrame(np.array([[None, i, row['media_id'],
+                                                  None, None, None, None, None,
+                                                  None, 1, user_gender,
+                                                  row['user_id'], None,
+                                                  row['user_age'], 0]]),
+                                       columns=['genre_id', 'ts_listen',
+                                                'media_id', 'album_id',
+                                                'context_type',
+                                                'release_date',
+                                                'platform_name',
+                                                'platform_family',
+                                                'media_duration',
+                                                'listen_type', 'user_gender',
+                                                'user_id', 'artist_id',
+                                                'user_age',
+                                                'is_listened'])
+                    deezer = deezer.append(new)
+        elif 'Anytime' in time:
+            for i in [1480513129, 1479067262, 1478675619]:
+                new = pd.DataFrame(np.array([[None, i, row['media_id'], None,
+                                              None, None, None, None, None, 1,
+                                              user_gender,
+                                              row['user_id'], None,
+                                              row['user_age'], 1]]),
+                                   columns=['genre_id', 'ts_listen',
+                                            'media_id', 'album_id',
+                                            'context_type',
+                                            'release_date', 'platform_name',
+                                            'platform_family',
+                                            'media_duration',
+                                            'listen_type', 'user_gender',
+                                            'user_id', 'artist_id', 'user_age',
+                                            'is_listened'])
+                deezer = deezer.append(new)
+        else:
+            t_dict = {'Morning': 0, 'Afternoon': 0, 'Evening': 0}
+            for t in time:
+                t_dict[t] = 1
+            for i in [('Morning', 1480513129), ('Afternoon', 1479067262),
+                      ('Evening', 1478675619)]:
+                new = pd.DataFrame(np.array([[None, i[1], row['media_id'],
+                                              None, None, None, None, None,
+                                              None, 1, user_gender,
+                                              row['user_id'], None,
+                                              row['user_age'], t_dict[i[0]]]]),
+                                   columns=['genre_id', 'ts_listen',
+                                            'media_id', 'album_id',
+                                            'context_type',
+                                            'release_date', 'platform_name',
+                                            'platform_family',
+                                            'media_duration',
+                                            'listen_type', 'user_gender',
+                                            'user_id', 'artist_id', 'user_age',
+                                            'is_listened'])
+                deezer = deezer.append(new)
+
+    return deezer
+
+
 if __name__ == "__main__":
     path = "my/path/to/dataset"
     # Load data
