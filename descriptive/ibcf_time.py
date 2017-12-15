2fb3f5775d287ea6a3f3587c78bfe7acea2471d2
AndPotap <apotapczynski@gmail.com>
2017-12-15 07:52:15 -0500

    created script to run IBCF model in each time of the day

--- descriptive/ibcf_time.py
+++ descriptive/ibcf_time.py
@@ -3,42 +3,97 @@
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 import pandas as pd
 from utils.Pipeline import Pipeline
+from models.model_based.matrix_creation import hit_rate_matrix_popular_items
 from models.neighborhood_based.ItemBasedCF import ItemBasedCF
-# from utils.similarity import ochiai
 from models.cross_validation.ModelTester import ModelTester
 from models.cross_validation.parameter_test import parameter_test
 import utils.loss_functions as lf
+from utils.preprocessing import get_moment_of_day2
+# from utils.similarity import ochiai
 # =========================================================================
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Load data
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+a = pd.DataFrame()  # to avoid "error message" of not using pandas
+# file = "./descriptive/db_nrows.csv"
+file = "./descriptive/train_sample.csv"
 # data_file = "./descriptive/db_nrows.csv"
 # data_file = "./descriptive/train.csv"
-spotify_file = "./descriptive/SpotifyAudioFeatures_clean.csv"
-sample_path = "./descriptive/train_sample.csv"
-data_file = "./descriptive/train_sample02.csv"
-pipe = Pipeline(deezer_path=data_file,
-                spotify_path=spotify_file,
-                sample_path=sample_path,
-                use_sample=False)
-a = pd.DataFrame()  # to avoid "error message" of not using pandas
-df = pipe.make_selected_few()
-print(df.shape)
-df.is_copy = False
-df = df.reset_index()
-df["user_time"] = df["user_id"].astype(str)+"_&_"+df["moment_of_day"]
-df["user_time_id"] = df.index
+# data_file = "./descriptive/train_sample02.csv"
+# data_file = "./descriptive/train_sample.csv"
+# spotify_file = "./descriptive/SpotifyAudioFeatures_clean.csv"
+# sample_path = "./descriptive/train_sample.csv"
+# pipe = Pipeline(deezer_path=data_file,
+#                 spotify_path=spotify_file,
+#                 sample_path=sample_path,
+#                 use_sample=False)
+# df = pipe.make_selected_few()
+# print(df.shape)
+df = pd.read_csv(file)
+df["moment_of_day"] = df["ts_listen"].apply(get_moment_of_day2)
 print(df.head())
-user = "user_time_id"
+# =========================================================================
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Do gigant matrix approach
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# df.is_copy = False
+# df = df.reset_index()
+# df["user_time"] = df["user_id"].astype(str)+"_&_"+df["moment_of_day"]
+# df["user_time_id"] = df.index
+# print(df.head())
+# user = "user_time_id"
+# songs = "media_id"
+# y = "is_listened"
+# data = df[[user, songs, y]]
+# print(data.head())
+# matrix = data.pivot_table(index=user,
+#                           columns=songs,
+#                           values=y,
+#                           aggfunc="mean")
+# print(matrix.head())
+# =========================================================================
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Do one ran per time of day approach
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+user = "user_id"
 songs = "media_id"
 y = "is_listened"
-data = df[[user, songs, y]]
-print(data.head())
-matrix = data.pivot_table(index=user,
-                          columns=songs,
-                          values=y,
-                          aggfunc="mean")
-print(matrix.head())
+# k_vals = [10, 20]
+k_vals = [25]
+cv_times = 1
+print("\nRunning the model wo tod segmentation")
+matrix = hit_rate_matrix_popular_items(data=df)
+# matrix = data.pivot_table(index=user,
+#                           columns=songs,
+#                           values=y,
+#                           aggfunc="mean")
+result_test, result_train = parameter_test(k_vals,
+                                           ItemBasedCF,
+                                           ModelTester(),
+                                           lf.mean_squared_error,
+                                           matrix,
+                                           cv_times,
+                                           verbose=True)
+
+tod_list = ['morning', 'afternoon_evening', 'late_night']
+for tod in tod_list:
+    print("\nCurrently on: "+tod)
+    df = df[df["moment_of_day"] == tod]
+    data = df[[user, songs, y]]
+    print(data.head())
+    matrix = hit_rate_matrix_popular_items(data=df)
+    # Below is the code in case that we want to use Pipeline
+    # matrix = data.pivot_table(index=user,
+    #                           columns=songs,
+    #                           values=y,
+    #                           aggfunc="mean")
+    result_test, result_train = parameter_test(k_vals,
+                                               ItemBasedCF,
+                                               ModelTester(),
+                                               lf.mean_squared_error,
+                                               matrix,
+                                               cv_times,
+                                               verbose=True)
 # =========================================================================
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Run IBCF model
@@ -51,13 +106,13 @@ print(matrix.head())
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Evaluate the model
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-k_vals = [5, 10]
-cv_times = 1
-result_test, result_train = parameter_test(k_vals,
-                                           ItemBasedCF,
-                                           ModelTester(),
-                                           lf.mean_squared_error,
-                                           matrix,
-                                           cv_times,
-                                           verbose=True)
+# k_vals = [10, 20]
+# cv_times = 1
+# result_test, result_train = parameter_test(k_vals,
+#                                            ItemBasedCF,
+#                                            ModelTester(),
+#                                            lf.mean_squared_error,
+#                                            matrix,
+#                                            cv_times,
+#                                            verbose=True)
 # =========================================================================
